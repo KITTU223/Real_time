83dfb64e1f86a43e78e6af75d9701a38f157ae5e
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2021-12-25 11:35:48 +0530

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path')
+const expresslayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 3000
+
+app.get('/',(req,res) =>{
+    res.render('home')
+});
+
+app.use(expresslayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs')
+
+app.listen(PORT, () =>{
+    console.log(`Listning on port ${PORT}`);
+});
