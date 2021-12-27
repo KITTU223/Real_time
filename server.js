526504bfb7957ef148300df7fad2342d7472228c
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2021-12-27 12:54:58 +0530

    add cart,auth

--- server.js
+++ server.js
@@ -7,13 +7,23 @@ const PORT = process.env.PORT || 3000
 
 //assets
 app.use(express.static('public'));
-app.get('/',(req,res) =>{
-    res.render('home')
-});
 
 app.use(expresslayout);
 app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res) =>{
+    res.render('home')
+});
+app.get('/cart',(req,res) =>{
+    res.render('customers/cart')
+});
+app.get('/login',(req,res) =>{
+    res.render('auth/login')
+});
+app.get('/register',(req,res) =>{
+    res.render('auth/register')
+});
 
 app.listen(PORT, () =>{
     console.log(`Listning on port ${PORT}`);
