3fd17549686daeddb63e9a268c16c411b028ff63
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2021-12-31 22:06:12 +0530

    login logout

--- app/http/controllers/authConroller.js
+++ app/http/controllers/authConroller.js
@@ -9,6 +9,12 @@ function authController(){
             res.render('auth/login')
         },  
         postLogin(req, res, next){
+            const {  email, password  } = req.body
+            //validate request
+            if(!email || !password){
+                req.flash('error','All fields are required')
+                return res.redirect('/login')
+            }
             passport.authenticate('local', (err, user, info) =>{
                 if(err){
                     req.flash('error',info.message)
