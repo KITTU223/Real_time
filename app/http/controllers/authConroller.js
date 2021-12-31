8f9de212eb13366d614bace5eedcd3d94b5657c5
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2021-12-31 22:00:00 +0530

    login logout

--- app/http/controllers/authConroller.js
+++ app/http/controllers/authConroller.js
@@ -1,10 +1,75 @@
+const User = require('../../models/user')
+const bcrypt = require('bcrypt')
+const { redirect } = require('express/lib/response')
+const passport = require('passport')
+
 function authController(){
     return{
         login(req,res){
             res.render('auth/login')
+        },  
+        postLogin(req, res, next){
+            passport.authenticate('local', (err, user, info) =>{
+                if(err){
+                    req.flash('error',info.message)
+                    return next(err)
+                }
+                if(!user){
+                    req.flash('error',info.message)
+                    return res.redirect('/login')
+                }
+                req.logIn(user, (err) =>{
+                    if(err){
+                        req.flash('error',info.message)
+                        return next(err)
+                    }
+                return res.redirect('/')
+                })
+            })(req,res,next)
         },
         register(req,res){
             res.render('auth/register')
+        },
+        async postRegister(req,res){
+            const { name, email, password  } = req.body
+            //validate request
+            if(!name || !email || !password){
+                req.flash('error','All fields are required')
+                req.flash('name',name)
+                req.flash('email',email)
+                return res.redirect('/register')
+            }
+
+            //chcek if email exist
+            User.exists({ email: email}, (err, result) =>  {
+                if(result){
+                    req.flash('error','Email already exist')
+                    req.flash('name',name)
+                    req.flash('email',email)
+                    return res.redirect('/register')
+                }
+            })
+
+            //hash password
+            const hashedPassword = await bcrypt.hash(password, 10)
+            //create user
+            const user = new User({
+                name,
+                email,
+                password: hashedPassword
+            })
+            user.save().then((user) =>{
+                //login 
+                return res.redirect('/')
+            }).catch(err =>{
+                req.flash('error','Something went wrong')
+                return res.redirect('/register')
+            })
+
+        },
+        logout(req,res){
+            req.logout()
+            res.redirect('/login')
         }
     }
 }
--- server.js
+++ server.js
@@ -9,12 +9,13 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('express-flash');
 const MongoDbStore = require('connect-mongo')(session)
+const passport = require('passport')
 
 
 app.use(express.static('public'));
 
 
-//globle middewares
+
 
 //database
 const url = 'mongodb://localhost/pizza';
@@ -25,7 +26,9 @@ connection.once('open', () =>{
 }).catch(err => {
         console.log('Connection failed...')
 
-    });
+});
+
+
 //session store
 let mongoStore = new MongoDbStore({
     mongooseConnection: connection,
@@ -40,11 +43,21 @@ app.use(session({
     cookie:{maxAge:1000*60*60*24} //24 hours
 }))
 
+//passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 app.use(flash());   
+app.use(express.urlencoded({extended:false}))
 app.use(express.json());
 
+//globle middewares
 app.use((req,res,next) =>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
