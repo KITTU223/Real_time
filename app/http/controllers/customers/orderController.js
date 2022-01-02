29b13d04a1ec6435abf66583b56f5f0aa37d3432
Krutarth Chauhan <63405029+KITTU223@users.noreply.github.com>
2022-01-02 16:09:48 +0530

    add orders table,placed orders

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -30,7 +30,7 @@ function orderController(){
                 {sort:{ 'createdAt': -1 } } )
             res.header('Cache-Control','no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
             res.render('customers/orders',{orders: orders, moment: moment})
-            console.log(orders) 
+            // console.log(orders) 
         }
     }
 }
